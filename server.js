f45487d1fbec878b5cdd84b31f6202676fd666ee
Haripriya866 <haripriyakanike5643@gmail.com>
2024-11-25 18:54:53 +0530

    Replaced bcrypt with bcryptjs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
 const uuid = require("uuid");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 const app = express();
